c5e5cb71c5cbedd3d131e584991b82250d484266
Bradley J. Bottomlee <bradley.bttm@gmail.com>
2019-07-21 23:11:08 -0400

    Slight changes

--- scraper.py
+++ scraper.py
@@ -21,21 +21,16 @@ def scraper(username):
     # grab table
     table = browser.find_element_by_xpath('//*[@id="content"]/div/div/section/table/tbody')
     rows = table.find_elements_by_tag_name('tr')  # grab all rows
-    rows_length = len(rows)
 
-    for i in range(0, rows_length):
-        user_friend = rows[i]
-        new_friend = build_friend(user_friend, browser)
-
+    for row in rows:
+        user_friend = row
+        browser2 = webdriver.Chrome(options=browser_profile)  # launch new window for friend
+        new_friend = build_friend(user_friend, browser2)
+        browser2.close()
         # if no ratings are found in given friend, build_friend() returns None
         # hence, we check if it is None before appending
         if new_friend is not None:
             friends.append(new_friend)
-
-        # we need to navigate back and re-grab table, rows
-        browser.get(following_url)
-        table = browser.find_element_by_xpath('//*[@id="content"]/div/div/section/table/tbody')
-        rows = table.find_elements_by_tag_name('tr')
 
         sleep(1)
 
@@ -43,43 +38,49 @@ def scraper(username):
 
 
 def build_friend(some_friend, browser):
-    # navigate to friend's ratings page
     cols = some_friend.find_elements_by_tag_name('td')
     ratings_link = cols[1].find_element_by_tag_name('a').get_attribute('href')
     ratings_link_2 = ratings_link + "ratings/"
-    browser.get(ratings_link_2)
-    films_ratings = dict()
 
-    # ensure there are ratings present
-    # No ratings will only occur when user has seen 0 films
-    if (no_ratings(browser)):
-        return None
+    while True:  # sometimes an exception is thrown during this part. If this is the case, simply retry
+        try:
+            # navigate to friend's ratings page
+            browser.get(ratings_link_2)
+            films_ratings = dict()
 
-    # maintain infinite loop until no more pages are found
-    while True:
+            # ensure there are ratings present
+            # No ratings will only occur when user has seen 0 films
+            if no_ratings(browser):
+                return None
 
-        # get main section of films and collect data
-        film_section = browser.find_element_by_xpath('//*[@id="content"]/div/div/section/ul')
-        films = film_section.find_elements_by_tag_name('li')
+            # maintain infinite loop until no more pages are found
+            while True:
 
-        # iterate through films on current page
-        for film in films:
-            div = film.find_element_by_tag_name('div')
-            film_name = div.get_attribute('data-film-name')
-            ratings_div = film.find_element_by_tag_name('p').find_element_by_tag_name('span')
-            rating = ratings_div.text
-            rating = transform_ratings(rating)
-            if rating == -1:
-                continue
-            films_ratings[film_name] = rating
+                # get main section of films and collect data
+                film_section = browser.find_element_by_xpath('//*[@id="content"]/div/div/section/ul')
+                films = film_section.find_elements_by_tag_name('li')
 
-        # click next page if present
-        if next_button(browser):
-            browser.find_element_by_xpath('//*[@id="content"]/div/div/section/div[2]/div[2]/a').click()
-        else:
-            break
+                # iterate through films on current page
+                for film in films:
+                    div = film.find_element_by_tag_name('div')
+                    film_name = div.get_attribute('data-film-name')
+                    ratings_div = film.find_element_by_tag_name('p').find_element_by_tag_name('span')
+                    rating = ratings_div.text
+                    rating = transform_ratings(rating)
+                    if rating == -1:
+                        continue
+                    films_ratings[film_name] = rating
 
-    # navigate back to original
-    some_list = ratings_link.split("/")
-    new_friend = Friend(some_list[3], films_ratings)
-    return new_friend
+                # click next page if present
+                if next_button(browser):
+                    browser.find_element_by_xpath('//*[@id="content"]/div/div/section/div[2]/div[2]/a').click()
+                    sleep(0.2)
+                else:
+                    break
+
+            # navigate back to original
+            some_list = ratings_link.split("/")
+            new_friend = Friend(some_list[3], films_ratings)
+            return new_friend
+        except:
+            continue
